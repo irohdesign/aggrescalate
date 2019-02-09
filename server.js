ec9f7c6acc59eeb44f83d5a93d18733160904991
Jalia Evans <irohdesign@outlook.com>
2019-02-09 11:44:29 -0500

    got the text responding based on profanity numbers. working with counting repeated usage of curse words

--- server.js
+++ server.js
@@ -2,22 +2,54 @@
 var client = Algorithmia.client('sim7Mx4n9rMzOwk+kcUidmfnlKs1');
 var apiKey = "simN6+IoTAeS3mFDo3rCfA4lnxv1";
 
+// global variable
+var profCount = 0;
+
 // grabbing text
 
 $("#userSubmit").on("click", function() {
     var userEntry = document.getElementById("userEntry").value;
     // console.log(userEntry);
 
+    profCount = 0;
+
+    // profanity API work
     var input = [userEntry,[],false];
 Algorithmia.client(apiKey)
     .algo("nlp/ProfanityDetection/1.0.0")
     .pipe(input)
     .then(function(output) {
-        var result = output.result
+        var result = output.result;
+
         var objectProps = Object.getOwnPropertyNames(result);
-        console.log(objectProps[0]);
+        // console.log(objectProps[0]);
+        console.log(result);
         
+        $.each(objectProps, function(index, value) {
+            console.log(value);
+            profCount++;
+        })
+
+        var returnText = $("<div>");
+        var returnInfo;
+        if(profCount > 1) {
+            profSyntax = "profanities"
+        } else {
+            profSyntax = "profanity"
+        }
+
+        if(profCount < 2) {
+            returnInfo = `You've used ${profCount} unique ${profSyntax}. Maybe add some variation to get the message across.`;
+        } else {
+            returnInfo = `You've used ${profCount} unique ${profSyntax}, maybe chill out.`;
+        } 
+        $(returnText).text(returnInfo);
+        $("#results").append(returnText);
     });
+
+  
+    $("#userEntry").val("");
+    
 });
 
 
